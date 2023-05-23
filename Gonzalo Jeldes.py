b1f41dd551b42b19dddfdee743ac0a3d982873b7
joshuajeldes <134403006+joshuajeldes@users.noreply.github.com>
2023-05-23 10:05:01 -0400

    Add files via upload

---
+++ Gonzalo Jeldes.py
@@ -0,0 +1,13 @@
+op=0
+total=0
+print("bienbenido a El Diente de Oro")
+while True:
+        print("1)Carillas Porcelana:250.000")
+        print("2)Implantes Dentales:475.000")
+        print("3)Ortodoncia Brackets:800.000")
+        print("4)desea salir de la compra?")
+        op=int(input(f"inpgrese una opcion:"))
+        print("ingrese una opcion valida")
+          
+
+        
